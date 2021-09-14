6660233145e714082445148465c0caf0a501d39c
Robergwillian <roberg.willian@gmail.com>
2021-09-13 21:36:16 -0300

    Adicionado a funçao para finalizar o jogo quando a cabeca bate no corpo

--- script.js
+++ script.js
@@ -45,6 +45,13 @@ function iniciarJogo() {
   if (snake[0].y > 15 * box && direction == 'down') snake[0].y = 0
   if (snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box
 
+  for (i = 1; i < snake.length; i++) {
+    if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+      clearInterval(jogo)
+      alert('Game Over :(')
+    }
+  }
+
   criarBG()
   drawFood()
   criarCobrinha()
@@ -56,8 +63,6 @@ function iniciarJogo() {
   if (direction == 'left') snakeX -= box
   if (direction == 'up') snakeY -= box
   if (direction == 'down') snakeY += box
-
-  // snake.pop()
 
   if (snakeX != food.x || snakeY != food.y) {
     snake.pop()
